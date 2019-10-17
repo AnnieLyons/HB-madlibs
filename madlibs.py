390de7f0d93560e6fb8a043e6d37ceda15c49948
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-16 17:41:26 -0700

    Madlib lab finished

--- madlibs.py
+++ madlibs.py
@@ -33,21 +33,43 @@ def play_game():
     """Ask user to play game."""
 
     player = request.args.get("person")
-
-    return render_template("greet.html",
-                           person=player)
-
-@app.route('/game')
-def greet_person():
-    """Greet user with compliment."""
-
-    player = request.args.get("person")
-
     compliment = choice(AWESOMENESS)
 
-    return render_template("compliment.html",
+    return render_template("greet.html",
                            person=player,
                            compliment=compliment)
+
+@app.route('/goodbye')
+def say_goodbye():
+    """Says goodbye"""
+    no_play = request.args.get("play_game")
+    play = request.args.get("play_game")
+    # player = request.args.get("person")
+    # if no_play:
+    if no_play == "No":
+        return render_template("goodbye.html",
+                            # person=player,
+                            yesno=no_play)
+    else:
+        return render_template("lets_play.html",
+                            # person=player,
+                            yesno=play)
+
+
+@app.route('/madlib')
+def madlib():
+    """Plays madlib game."""
+    person = request.args.get("person")
+    color = request.args.get("color")
+    noun = request.args.get("noun")
+    adjective = request.args.get("adjective")
+
+
+    return render_template("madlib.html",
+                            person=person,
+                            color=color,
+                            noun=noun,
+                            adjective=adjective)
 
 
 if __name__ == '__main__':
@@ -55,3 +77,26 @@ if __name__ == '__main__':
     # "reloads" our web app if we change the code.
 
     app.run(debug=True)
+
+
+
+# @app.route('/game')
+# def greet_person():
+#     """Greet user with compliment."""
+
+#     player = request.args.get("person")
+
+#     compliment = choice(AWESOMENESS)
+
+#     return render_template("compliment.html",
+#                            person=player,
+#                            compliment=compliment)
+
+
+
+    # return render_template("goodbye.html",
+    #                         person=player,
+    # if no_play:
+    #     return render_template("goodbye.html",                      
+    #                             yes=play,
+    #                             no=no_play)
